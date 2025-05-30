e19be65ee889a6e06c389955764381ede937a98b
James Kim <yt4921@gmail.com>
2025-05-29 17:56:37 -0700

    to modify encrypt and decrypt, create the biMap function as I did in cmpt225 assignment 2

--- logInValidation.js
+++ logInValidation.js
@@ -3,51 +3,12 @@ var password;
 
 const dataBase = [
     { email: 'yt4921@gmail.com', password: 'abc' },
-    { email: 'nam@gmail.com',   password: '123'  },
-    { email: 'ykkk@gmail.com',     password: '123'  }
+    { email: 'nam@gmail.com', password: '123' },
+    { email: 'ykkk@gmail.com', password: '123' }
 ]
 
-function encrypt(){
-    const inputText = document.getElementById("password").value;
-    let encryptedText = "";
-    const key = 5;
-    for(let i=0; i<inputText.length;i++){
-        const charCode = inputText.charCodeAt(i);
-        if(charCode >= 65 && charCode <= 90){
-            encryptedText += String.fromCharCode(((charCode - 65 + key) % 26) + 65);
-        }
-        else if(charCode >= 97 && charCode <= 122){
-            encryptedText += String.fromCharCode(((charCode - 97 + key) % 26) + 97);
-        }
-        else{
-            encryptedText += inputText[i];
-        }
-    }
-    document.getElementById("password").value = encryptedText;
-    console.log("encrypted text: ",encryptedText );
-}
 
-function decrypt(){
-    const inputText = document.getElementById("password").value;
-    let decryptedText = "";
-    const key = 5;
-    for(let i=0; i<inputText.length;i++){
-        const charCode = inputText.charCodeAt(i);
-        if(charCode >= 65 && charCode <= 90){
-            decryptedText += String.fromCharCode(((charCode - 65 - key + 26) % 26) + 65);
-        }
-        else if(charCode >= 97 && charCode <= 122){
-            decryptedText += String.fromCharCode(((charCode - 97 - key + 26) % 26) + 97);
-        }
-        else{
-            decryptedText += inputText[i];
-        }
-    }
-    document.getElementById("password").value = decryptedText;
-    console.log("decrypted text: ",decryptedText );
-}
-
-function getUserInfo(event){
+function getUserInfo(event) {
     event.preventDefault();
     email = document.getElementById("email").value;
     password = document.getElementById("password").value;
@@ -56,7 +17,7 @@ function getUserInfo(event){
 
     var user = dataBase.find(u => u.email === email);
 
-    if(!user){
+    if (!user) {
         console.error("User info is not found");
         alert("User info is not found");
     }
@@ -65,9 +26,33 @@ function getUserInfo(event){
         console.error("Wrong password");
         alert("Wrong password");
     }
-    else{
+    else {
         console.log("Log in success");
         $("#blackRoundBox").hide();
         $("#alternativeTextBox").text("Log in success");
     }
+}
+
+
+
+const encryptText = {};
+const decryptText = {};
+
+
+
+function biMap() {
+    const plainText = 'abcdefghijklmnopqrstuvwxyz';
+    const reversedText = 'zyxwvutsrqponmlkjihgfedcba';
+
+    for (let i = 0; i < plainText.length; i++) {
+        encryptText[plainText[i]] = reversedText[i];
+        decryptText[reversedText[i]] = plainText[i];
+    }
+
+    for (let i = 0; i < plain.length; i++) {
+        encryptionMap[plain[i].toUpperCase()] = cipher[i].toUpperCase();
+        decryptionMap[cipher[i].toUpperCase()] = plain[i].toUpperCase();
+    }
+
+    
 }
